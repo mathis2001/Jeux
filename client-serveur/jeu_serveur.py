adc02a0899a10bc11ddcc7f030892c28e1ae6f4e
mathis2001 <40497633+mathis2001@users.noreply.github.com>
2021-11-23 12:50:31 +0100

    Update jeu_serveur.py

--- client-serveur/jeu_serveur.py
+++ client-serveur/jeu_serveur.py
@@ -15,18 +15,22 @@ print("le nombre à trouver est:", nbr_max)
 s_client.send(str(nbr_max).encode())
 nbr_u=0
 c=0
+
 while(nbr_u != nbr_max):
     s_client.send("Entrez un nombre".encode())
     nbr_u=int(s_client.recv(512))
     c=c+1
+    
     if(nbr_u>nbr_max):
         s_client.send("moins".encode())
         data=s_client.recv(512)
         print(data.decode())
+        
     elif(nbr_u<nbr_max):
         s_client.send("plus".encode())
         data=s_client.recv(512)
         print(data.decode())
+        
 s_client.send("win".encode())
 s_client.close()
 s.close()
