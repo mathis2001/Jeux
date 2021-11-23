968f0dd889e2ad95a742b01cdc5b1addfaeb4c02
mathis2001 <40497633+mathis2001@users.noreply.github.com>
2021-11-23 12:49:06 +0100

    Add files via upload

---
+++ client-serveur/jeu_serveur.py
@@ -0,0 +1,32 @@
+import socket
+import random
+    
+host="127.0.0.7"
+port=1337
+s=socket.socket()
+s.bind((host,port))
+s.listen()
+s_client,adresse=s.accept()
+s_client.send("Veuillez entrer le nombre maximum voulu:\n".encode())
+data=s_client.recv(512)
+print(data.decode())
+nbr_max=random.randint(1,int(data))
+print("le nombre à trouver est:", nbr_max)
+s_client.send(str(nbr_max).encode())
+nbr_u=0
+c=0
+while(nbr_u != nbr_max):
+    s_client.send("Entrez un nombre".encode())
+    nbr_u=int(s_client.recv(512))
+    c=c+1
+    if(nbr_u>nbr_max):
+        s_client.send("moins".encode())
+        data=s_client.recv(512)
+        print(data.decode())
+    elif(nbr_u<nbr_max):
+        s_client.send("plus".encode())
+        data=s_client.recv(512)
+        print(data.decode())
+s_client.send("win".encode())
+s_client.close()
+s.close()
